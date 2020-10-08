7aa1328a3928b9a3f8d099698b860bf6f2fa0839
cvasquez23 <chris@chrisvasquez.dev>
2020-10-08 16:15:04 -0400

    contact-form

---
+++ contact-form.php
@@ -0,0 +1,25 @@
+<?php
+$message_sent = false;
+if (isset($_POST['email']) && $_POST['email'] != '') {
+  if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    //submit form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $from = 'From: Portfolio Site';
+    $to = 'chris@chrisvasquez.dev';
+    $subject = 'Message from chrisvasquez.dev';
+    $body = '';
+
+    $body .= 'From: ' . $name . "\r\n";
+    $body .= 'Email: ' . $email . "\r\n";
+    $body .= 'Message: ' . $message . "\r\n";
+
+    mail($to, $subject, $body, $from);
+
+    $message_sent = true;
+  }
+}
+
+?>
